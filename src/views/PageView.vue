808fbd2205ca59b546518ab1d6bbadf6cb03aecb
Turgay Kivrak <turgay.kivrak@aurea.com>
2023-02-09 16:11:03 +0000

    b

--- src/views/PageView.vue
+++ src/views/PageView.vue
@@ -1,18 +1,43 @@
 <script setup lang="ts">
 
-defineProps<{
+import { ref, computed } from 'vue'
 
-}>()
+import { useRoute } from 'vue-router';
+
+const route = useRoute();  
+const pageNum: number = +route.params.pageno
+
 
 function getPage(pageNo:number) {
     return "/rube/rube_" +pageNo + ".html"
-  }
+}
+
+let rubeHtml
+function loadHTML(pageNo:number) {
+  return fetch(getPage(pageNo))
+            .then( (resp) => {
+                if(resp.status === 200){
+                    return resp.text();
+                }
+            })
+            .then( (data) => {
+                console.log(data);
+                rubeHtml = data;
+                return data
+            })
+            .catch( (error) => {
+                console.log(error);
+            })
+}
+
+//const rubeHtml = async () => await loadHTML(pageNum);
 
 </script>
 <template>
   <div class="row pt-5">
     <div class="float-end text-center"> sayfa {{ $route.params.pageno }} </div>
-    <object width='100%' height="800" type="text/html" :data="getPage($route.params.pageno)"></object>
+    <div v-html="rubeHtml"></div>
+    <!--<object width='100%' height="800" type="text/html" :data="getPage($route.params.pageno)"></object> -->
   </div>
 </template>
 
