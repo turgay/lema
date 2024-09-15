6b7e5e27c78a5dda87a5e515fd49fcf69de0a0bc
Turgay-Kivrak-o7s_nbcuni <turgay.kivrak@sky.uk>
2024-09-15 22:20:12 +0100

    More mobile fix: footer

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -131,7 +131,7 @@ function ClientLayoutContent({ children }: { children: React.ReactNode }) {
                     <div className="flex-grow w-full">
                         {children}
                     </div>
-                    <footer className="mt-8 p-4 sm:p-8 flex flex-col sm:flex-row items-center justify-between w-full max-w-3xl mx-auto text-sm text-gray-500 space-y-2 sm:space-y-0">
+                    <footer className="mt-8 p-4 sm:p-8 flex flex-row items-center justify-between w-full max-w-3xl mx-auto text-sm text-gray-500">
                         <div>2024 🍃 Lema</div>
                         <div className="flex space-x-4">
                         <a href="#" className="hover:text-gray-700">
