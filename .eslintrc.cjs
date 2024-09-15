0b9e2ba3be5179b908a0c93fda31d2f517e7c21d
Turgay-Kivrak-o7s_nbcuni <turgay.kivrak@sky.uk>
2024-09-15 21:20:06 +0100

    build settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,22 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
-  ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
-  ],
+  extends: ['next/core-web-vitals', 'eslint:recommended'],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  // Add any custom rules here
+};
